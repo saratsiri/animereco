a6d6165641b5b26c15ccd10f8f874e3b29aa2ff8
Sarat Sirirachatapong <sarat.fin@gmail.com>
2023-05-29 10:41:00 +0900

    Implemented the local ver to cloud ver

--- anime_recommender/streamlit_recommender_cloud.py
+++ anime_recommender/streamlit_recommender_cloud.py
@@ -36,33 +36,30 @@ AnimesDF = load_csv_from_gcs(bucket_name, "anime_cleaned.csv")
 loaded_model = load_pickle_from_gcs(bucket_name, "baseline_model.pickle")
 loaded_knn_model = load_pickle_from_gcs(bucket_name, "knn_model.pickle")
 
-import re
-
 def get_item_recommendations(algo, algo_items, anime_title, anime_id=100000, k=20):
     try:
+        # Check if the input is empty or consists of only spaces
+        if not anime_title or anime_title.isspace():
+            st.write("Enter something you neckbeard! There's no empty anime name.")
+            return
+
         anime_title = anime_title.strip().lower()
 
-        # Create regex pattern
-        pattern = '.*'.join(anime_title)  # Converts 'slamdunk' to 's.*l.*a.*m.*d.*u.*n.*k'
-        regex = re.compile(pattern)  # Compiles a regex pattern which can match any characters between the letters of 'slamdunk'
-
-        # Check if the regex pattern is in the title
-        matching_animes = AnimesDF[AnimesDF['title'].str.lower().apply(lambda x: bool(regex.search(x)))]
+        # Check if the title contains the anime_title as a substring
+        matching_animes = AnimesDF[AnimesDF['title_lower'].str.contains(anime_title)]
 
         # If no results, search by the English title
         if matching_animes.empty:
-            matching_animes = AnimesDF[AnimesDF['title_english'].str.lower().apply(lambda x: bool(regex.search(x)))]
+            matching_animes = AnimesDF[AnimesDF['title_english_lower'].str.contains(anime_title)]
+            if matching_animes.empty:
+                st.write("No matching anime found. Please check your input.")
+                return
 
-        if matching_animes.empty:
-            st.write("No matching anime found. Please check your input.")
-            return
-
-        # If there are multiple matches, select the best one
-        if len(matching_animes) > 1:
-            st.write("Assuming you meant: ", matching_animes.iloc[0]['title'])
-            anime_id = matching_animes.iloc[0]['anime_id']
-        else:
-            anime_id = matching_animes['anime_id'].iloc[0]
+        # If there are multiple matches, select the best one (the one with the shortest title)
+        best_match_index = matching_animes['title'].str.len().idxmin()
+        best_match = matching_animes.loc[best_match_index]
+        st.write("Assuming you meant: ", best_match['title'])
+        anime_id = best_match['anime_id']
 
         iid = algo_items.trainset.to_inner_iid(anime_id)
         neighbors = algo_items.get_neighbors(iid, k=k)
@@ -84,7 +81,7 @@ def get_item_recommendations(algo, algo_items, anime_title, anime_id=100000, k=2
         st.markdown(table_md)
 
     except Exception as e:
-        st.write("Stop trying to debug this code, NECKBEARD!")
+        st.write("No matching anime found. Please check your input.")
 
 # Set up the Streamlit interface
 st.title('Weeaboo Wonderland')
@@ -96,8 +93,6 @@ You can click on the title of any anime in the recommendations to go to its webp
 
 anime_title = st.text_input('Please enter an anime title')
 
-# Check if the input is empty or consists of only spaces
-if not anime_title or anime_title.isspace():
-    st.write("Enter something you neckbeard! There's no empty anime name.")
-else:
+# Only run the recommendation function if the user has entered something
+if anime_title:
     get_item_recommendations(loaded_model, loaded_knn_model, anime_title)
