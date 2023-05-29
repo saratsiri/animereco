1d0a1558cfac02f1923a0e649ef084fe2d7756a2
Sarat Sirirachatapong <sarat.fin@gmail.com>
2023-05-29 10:31:46 +0900

    Fixed multiple bugs: 1) best match = shortest title name (Better matching capability) 2)optimize run time via another preprocessing step 3)Reconfig regex so it doesnt just match letters but matches words in the title.

--- anime_recommender/streamlit_recommender.py
+++ anime_recommender/streamlit_recommender.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 import os
+import re
 
 # Load the models and data when the script starts
 AnimesDF = pd.read_csv(f'{os.getcwd()}/raw_data/anime_cleaned.csv')
@@ -12,44 +13,43 @@ with open(f"{os.getcwd()}/anime_recommender/trained_models/baseline_model.pickle
 with open(f"{os.getcwd()}/anime_recommender/trained_models/knn_model.pickle", 'rb') as f:
     loaded_knn_model = pickle.load(f)
 
+import re
+
+# Preprocessing step
+AnimesDF['title_lower'] = AnimesDF['title'].str.lower()
+AnimesDF['title_english_lower'] = AnimesDF['title_english'].str.lower()
+
 def get_item_recommendations(algo, algo_items, anime_title, anime_id=100000, k=20):
     try:
+        if not anime_title or anime_title.isspace():
+            st.write("Enter something you neckbeard! There's no empty anime name.")
+            return
+
         anime_title = anime_title.strip().lower()
 
-        # Create regex pattern
-        pattern = '.*'.join(anime_title)  # Converts 'slamdunk' to 's.*l.*a.*m.*d.*u.*n.*k'
-        regex = re.compile(pattern)  # Compiles a regex pattern which can match any characters between the letters of 'slamdunk'
+        matching_animes = AnimesDF[AnimesDF['title'].str.lower().str.startswith(anime_title)]
 
-        # Check if the regex pattern is in the title
-        matching_animes = AnimesDF[AnimesDF['title'].str.lower().apply(lambda x: bool(regex.search(x)))]
-
-        # If no results, search by the English title
         if matching_animes.empty:
-            matching_animes = AnimesDF[AnimesDF['title_english'].str.lower().apply(lambda x: bool(regex.search(x)))]
+            matching_animes = AnimesDF[AnimesDF['title_english'].str.lower().str.startswith(anime_title)]
 
         if matching_animes.empty:
             st.write("No matching anime found. Please check your input.")
             return
 
-        # If there are multiple matches, select the best one
-        if len(matching_animes) > 1:
-            st.write("Assuming you meant: ", matching_animes.iloc[0]['title'])
-            anime_id = matching_animes.iloc[0]['anime_id']
-        else:
-            anime_id = matching_animes['anime_id'].iloc[0]
+        best_match_index = matching_animes['title'].str.len().idxmin()
+        best_match = matching_animes.loc[best_match_index]
+        st.write("Assuming you meant: ", best_match['title'])
+        anime_id = best_match['anime_id']
 
         iid = algo_items.trainset.to_inner_iid(anime_id)
         neighbors = algo_items.get_neighbors(iid, k=k)
         raw_neighbors = (algo.trainset.to_raw_iid(inner_id) for inner_id in neighbors)
 
         df = pd.DataFrame(raw_neighbors, columns = ['Anime_ID'])
-        df = pd.merge(df, AnimesDF, left_on = 'Anime_ID', right_on = 'anime_id', how = 'left')
+        df = df.merge(AnimesDF[['anime_id', 'title', 'genre', 'score']], left_on='Anime_ID', right_on='anime_id', how='left')
 
-        df["Title"] = df.apply(lambda row: f"[{row['title']}](https://myanimelist.net/anime/{row['Anime_ID']}/{row['title'].replace(' ', '_')})", axis=1)
-        df["Genre"] = df["genre"]
-        df["Score"] = df["score"]
+        df["Title"] = "[" + df["title"] + "](https://myanimelist.net/anime/" + df['Anime_ID'].astype(str) + "/" + df['title'].str.replace(' ', '_') + ")"
 
-        # Create markdown tables
         table_md = "| Title | Genre | Score |\n| --- | --- | --- |\n"
         for i, row in df[:10].iterrows():
             table_md += f"| {row['Title']} | {row['Genre']} | {row['Score']} |\n"
@@ -58,8 +58,7 @@ def get_item_recommendations(algo, algo_items, anime_title, anime_id=100000, k=2
         st.markdown(table_md)
 
     except Exception as e:
-        st.write("Stop trying to debug this code, NECKBEARD!")
-
+        st.write("An error occurred during the processing. Please try again.")
 # Set up the Streamlit interface
 st.title('Weeaboo Wonderland')
 st.write("""
@@ -70,8 +69,6 @@ You can click on the title of any anime in the recommendations to go to its webp
 
 anime_title = st.text_input('Please enter an anime title')
 
-# Check if the input is empty or consists of only spaces
-if not anime_title or anime_title.isspace():
-    st.write("Enter something you neckbeard! There's no empty anime name.")
-else:
+# Only run the recommendation function if the user has entered something
+if anime_title:
     get_item_recommendations(loaded_model, loaded_knn_model, anime_title)
